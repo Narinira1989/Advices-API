89f138ad6876ae19e034f8d520a47555233b50e1
Irina <irinanarin1989@gmail.com>
2023-08-25 22:38:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import todoImage from './todo.jpg';
+
 
 function App() {
+
+  const containerStyle = {
+    backgroundImage: `url(${todoImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'top',
+    
+  };
+
+const [advice, setAdvise] = useState("");
+
+useEffect(() => {
+  getAdvice()
+},[])
+
+const getAdvice = async () => {
+  const response = await fetch ('https://www.boredapi.com/api/activity/');
+  const data = await response.json();
+  setAdvise(data.activity)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={containerStyle}>
+      <div className='container'>
+        <h1>Let's find you something to do</h1>
+      </div>
+
+      <div className='container'>
+        <p>{advice}</p>
+      </div>
+
+      <div className='container'>
+        <button onClick={getAdvice}>New advice</button>
+      </div>
     </div>
   );
 }
